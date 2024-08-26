f511465013d622ed86bce7d0cd748c78899524aa
Ruhi <ruhiresearcher209@gmail.com>
2024-08-26 18:00:12 +0000

    CI: Fix for musllinux

--- scripts/cibw_before_build.sh
+++ scripts/cibw_before_build.sh
@@ -3,8 +3,11 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
     brew install boost
+elif ldd --version 2>&1 | grep -q musl; then
+    # musllinux
+    apk add --no-cache boost-dev
 else
-    # Linux (assuming yum-based system)
+    # manylinux
     yum -y install boost-devel
 fi
 
